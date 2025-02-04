e48ca65a7393ca8d806ea5b83d33fcfe528fe677
Hardik Hathwal <132740042+Kidrah-kun@users.noreply.github.com>
2025-02-04 15:09:22 +0530

    Add files via upload
    making it fully functional and also adding better css design

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 const inputValue = document.getElementById("user-input");
+
+document.querySelectorAll(".numbers").forEach(function (item) {
+  item.addEventListener("click", function (e) {
+    if (inputValue.innerText === "NaN") {
+      inputValue.innerText = "";
+    }
+    if (inputValue.innerText === "0") {
+      inputValue.innerText = "";
+    }
+    inputValue.innerText += e.target.innerHTML.trim();
+  });
+});
+
+document.querySelectorAll(".operations").forEach(function (item) {
+  item.addEventListener("click", function (e) {
+    const lastChar = inputValue.innerText.charAt(inputValue.innerText.length - 1);
+
+    if (e.target.innerHTML === "=") {
+      try {
+        inputValue.innerText = eval(inputValue.innerText);
+      } catch (err) {
+        inputValue.innerText = "NaN";
+      }
+    } else if (e.target.innerHTML === "AC") {
+      inputValue.innerText = 0;
+    } else if (e.target.innerHTML === "DEL") {
+      inputValue.innerText = inputValue.innerText.slice(0, -1);
+      if (inputValue.innerText.length === 0) {
+        inputValue.innerText = 0;
+      }
+    } else {
+      if (lastChar !== "+" && lastChar !== "-" && lastChar !== "*" && lastChar !== "/") {
+        inputValue.innerText += e.target.innerHTML;
+      }
+    }
+  });
+});
